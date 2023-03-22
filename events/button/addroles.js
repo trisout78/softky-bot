c1896b5e4c34745898ec06322cf287346da8e321
trisout78 <81978700+trisout78@users.noreply.github.com>
2023-03-22 21:33:28 +0100

    Delete events directory

--- events/button/addroles.js
+++
@@ -1,9 +0,0 @@
-const bot = require("../../index");
-
-bot.on('interactionCreate', (interaction) => {
-    if(!interaction.isButton()) return;
-    if(interaction.customId != "btn-visiteur") return;
-
-    interaction.member.roles.add("1050516593437835334");
-    interaction.reply({content: "Vous avez maintenant accès au serveur.", ephemeral: true});
-});
